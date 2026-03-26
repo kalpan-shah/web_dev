4959cb47703f5da548d07ede05fa5dc9eaef8239
kalpanshah <kalpans@glidemtech.com>
2026-03-26 15:24:25 +0530

    Update: updated the test_health test as per the latest httpx library that requires to introduce a transport and doesn't handle the app connection directly

--- src/tests/test_health.py
+++ src/tests/test_health.py
@@ -1,11 +1,13 @@
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 from app.main import app
 
 import pytest
 
 @pytest.mark.asyncio
 async def test_health():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    # Define the transport with your app
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
         res = await ac.get("/api/v1/health")
         assert res.status_code == 200
         assert res.json() == {"status": "probably okay"}
