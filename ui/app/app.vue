cbbb5e64a4806f98cbc9d2fe6a75134e4116c87f
kalpanshah <kalpanshah106@gmail.com>
2026-05-26 18:28:20 +0530

    feat: initialized nuxt.js project

---
+++ ui/app/app.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <!-- the root wrapper -->
+  <UApp>
+    <UHeader>
+      <template #left>
+        <NuxtLink to="/">
+          <!-- Update with your logo -->
+        </NuxtLink>
+        <!-- TODO: Update with menu to switch between Todo and wishlist -->
+        <!-- <TemplateMenu /> -->
+      </template>
+
+      <template #right>
+        
+        <!-- TODO: Update with out github repo url -->
+        <UButton
+        to="https://github.com/nuxt-ui-templates/starter"
+        target="_blank"
+        icon="i-simple-icons-github"
+        aria-label="GitHub"
+        color="neutral"
+        variant="ghost"
+        />
+        <!-- TODO: Logout button -->
+        <!-- Will display a you have been logged out message
+        and then in 7 seconds will redirect to the login page  
+        -->
+        <UColorModeButton />
+      </template>
+    </UHeader>
+    <!-- the container for the primary content -->
+    <UMain>
+      <!-- so this directly routes to pages/index.vue, you can add more pages and link to them from the header menu 
+        1. default is the Todo, crud page and the second page is the wishlist page, linked from the header menu 
+      -->
+      <NuxtPage />
+    </UMain>
+
+    <USeparator icon="i-heroicons-fire" />
+
+    <UFooter>
+      <template #left>
+        <!-- TODO: Update name with github profile hyperlink -->
+        <p class="text-sm text-muted">
+          Built with Love❤️ By Kalpan Shah • © {{ new Date().getFullYear() }}
+        </p>
+      </template>
+
+      <template #right>
+        <!-- TODO: Update with something-->
+      </template>
+    </UFooter>
+  </UApp>
+</template>
