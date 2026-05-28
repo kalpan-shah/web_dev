fdc9c4252a58732bef02792f36b7ed64670d7caa
kalpan <kalpanshah106@gmail.com>
2026-05-28 18:59:18 +0000

    test: Added the required test package for coverage
     - Solved warning for default jwt secret key value

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
     ALLOWED_ORIGINS: list[str] = ["http://localhost:3000"]
 
     # 2. Authentication Variables
-    JWT_SECRET_KEY: str = "supersecretkey"
+    JWT_SECRET_KEY: str = "super_secret_key_it_must_be_atleast_32_characters_long_for_security"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15 # 15 minutes
 
