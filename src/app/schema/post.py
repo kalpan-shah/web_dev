7a1ff46999d4f2f974438348ade3ddf17b706872
kalpan <kalpanshah106@gmail.com>
2026-07-01 14:16:35 +0000

    WIP: Planning the API contract for creating, updating todos

--- src/app/schema/post.py
+++ src/app/schema/post.py
@@ -12,18 +12,33 @@ from datetime import datetime
 from uuid import UUID, uuid4
 
 
-class PostCreate(BaseModel):
+class TodoCreate(BaseModel):
     """
-        Create a New Post
+        Create a New Todo
     """
     title: str
-    content: str
+    items: list["TodoItemCreate"]  # List of TodoItemCreate objects
 
 
-class Post(PostCreate):
+class Todo(TodoCreate):
     """
-        Post Info Model
+        Todo Info Model
     """
     id: UUID
     user_id: UUID
     created_at: datetime
+
+class TodoItemCreate(BaseModel):
+    """
+        Create a New Todo Item
+    """
+    item: str
+    is_checked: bool = False  # Default to unchecked
+
+class TodoUpdate(Todo):
+    """
+        Update a Todo
+    """
+    title: str | None = None  # Optional title for update
+    items: list["TodoItemUpdate"] | None = None  # Optional list of TodoItemUpdate objects
+    
